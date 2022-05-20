cdf98bd351e61a0ddac682522e133937b02d9ca4
Robert Brunetto <robert@brunetto.cz>
2022-05-20 22:45:31 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿DateTime prevTime = DateTime.MinValue;
+var awakeTime = DateTime.Now;
+while (true)
+{
+    var time = DateTime.Now;
+    if (time.Subtract(prevTime) > TimeSpan.FromSeconds(1))
+    {
+        if (prevTime == DateTime.MinValue)
+            Console.WriteLine(time + "\tSleep monitoring app started");
+        else
+        {
+            var difference = time - prevTime;
+            var awake = prevTime - awakeTime;
+            awakeTime = DateTime.Now;
+            Console.WriteLine($"After being awake {awake}. PC sleeped {difference} from {prevTime} to {time}");
+        }
+    }
+    prevTime = time;
+    Task.Delay(500).Wait();
+}
